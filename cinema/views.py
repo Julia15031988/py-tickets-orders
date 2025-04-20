fecac277e7bcf7c3ee86ba40558f612fd2e6ed16
Julia Okhrymenko <Ohrimenko15@gmail.com>
2025-04-20 15:50:58 +0300

    solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets
-
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
-
+from rest_framework import viewsets, filters, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Q
+from rest_framework.permissions import IsAuthenticated
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order, Ticket
+from datetime import datetime
 from cinema.serializers import (
     GenreSerializer,
     ActorSerializer,
@@ -12,6 +14,8 @@ from cinema.serializers import (
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
     MovieListSerializer,
+    OrderCreateSerializer,
+    OrderSerializer,
 )
 
 
@@ -43,6 +47,21 @@ class MovieViewSet(viewsets.ModelViewSet):
 
         return MovieSerializer
 
+    def get_queryset(self):
+        queryset = Movie.objects.all()
+        genres = self.request.query_params.getlist("genres")
+        actors = self.request.query_params.getlist("actors")
+        title = self.request.query_params.get("title")
+
+        if genres:
+            queryset = queryset.filter(genres__id__in=genres)
+        if actors:
+            queryset = queryset.filter(actors__id__in=actors)
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        return queryset.distinct()
+
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
@@ -56,3 +75,37 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+    def get_queryset(self):
+        queryset = MovieSession.objects.all()
+        date = self.request.query_params.get("date")
+        movie_id = self.request.query_params.get("movie")
+
+        if date:
+            try:
+                date_obj = datetime.strptime(date, "%Y-%m-%d").date()
+                queryset = queryset.filter(show_time__date=date_obj)
+            except ValueError:
+                pass  # або raise validation error
+
+        if movie_id:
+            queryset = queryset.filter(movie_id=movie_id)
+
+        return queryset
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Order.objects.all()
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user).prefetch_related(
+            "tickets__movie_session__cinema_hall",
+            "tickets__movie_session__movie"
+        ).order_by("-created_at")
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return OrderCreateSerializer
+        return OrderSerializer
