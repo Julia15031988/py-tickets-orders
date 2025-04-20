fe7941f9e8ab217f3fb8ce64e77b573d8443a30e
Julia Okhrymenko <Ohrimenko15@gmail.com>
2025-04-20 15:54:02 +0300

    solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order, Ticket
+from cinema.models import Genre, Actor, CinemaHall,\
+    Movie, MovieSession, Order, Ticket
 
 
 class GenreSerializer(serializers.ModelSerializer):
--- cinema/views.py
+++ cinema/views.py
@@ -2,7 +2,8 @@ from rest_framework import viewsets, filters, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order, Ticket
+from cinema.models import Genre, Actor, CinemaHall,\
+    Movie, MovieSession, Order
 from datetime import datetime
 from cinema.serializers import (
     GenreSerializer,
